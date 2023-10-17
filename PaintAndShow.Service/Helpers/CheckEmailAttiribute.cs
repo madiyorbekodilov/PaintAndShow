f5a50d16bb950218265228fef116acfccf7e9bb8
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 00:40:24 +0500

    CheckEmailAttiribute added

---
+++ PaintAndShow.Service/Helpers/CheckEmailAttiribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using PaintAndShow.Service.Exceptions;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaintAndShow.Service.Helpers;
+
+public class CheckEmailAttiribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        string email = value.ToString()
+            ?? throw new CustomException(403, "Invalid email");
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        if (!Regex.IsMatch(email, pattern))
+            throw new CustomException(403, "Invalid email");
+        return true;
+    }
+}
--- PaintAndShow.Service/Helpers/PhotoWriter.cs
+++ PaintAndShow.Service/Helpers/PhotoWriter.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PaintAndShow.Service.Exceptions;
 
-namespace PaintAndShow.Service.Helpers
+namespace PaintAndShow.Service.Helpers;
+
+public class PhotoWriter
 {
-    internal class PhotoWriter
+    public string GetPhoto(string photoPath)
     {
+        string sourceImagePath = photoPath;
+
+        string saveDirectory = @"..\..\..\..\PaintAndShow.Data\AllMedia\";
+        string respons = "";
+        try
+        {
+            //Combine the save directory with the source image file name
+            string saveImagePath = Path.Combine(saveDirectory, Path.GetFileName(sourceImagePath));
+            respons = saveImagePath;
+
+            //Copy the image from the source path to the save path
+            File.Copy(sourceImagePath, saveImagePath, true);
+
+        }
+        catch (Exception ex)
+        {
+            throw new CustomException(400,ex.Message);
+        }
+
+        return respons;
     }
 }
