3f711fc7f13f54d47e33483fcc26ae877dc30b24
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-17 22:55:21 +0500

    Photo's entity added

---
+++ PaintAndShow.Domain/Entities/Photo.cs
@@ -0,0 +1,11 @@
+﻿using PaintAndShow.Domain.Commons;
+
+namespace PaintAndShow.Domain.Entities;
+
+public class Photo : Auditable
+{
+    public string PhotoName { get; set; }
+    public string? Description { get; set; }
+    public string Path { get; set; }
+    public long UserId { get; set; }
+}
