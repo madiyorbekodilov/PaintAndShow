19c52b00f525c4a9bf41a6cfc800833afa308c7b
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 00:45:19 +0500

    MappingProfile added

---
+++ PaintAndShow.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PaintAndShow.Domain.Entities;
+using PaintAndShow.Service.DTOs.Users;
+using PaintAndShow.Service.DTOs.Photos;
+
+namespace PaintAndShow.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // User
+        CreateMap<User, UserResultDto>().ReverseMap();
+        CreateMap<UserCreationDto, User>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+
+        // Photo
+        CreateMap<Photo, PhotoResultDto>().ReverseMap();
+        CreateMap<PhotoCreationDto, Photo>().ReverseMap();
+        CreateMap<PhotoUpdateDto, Photo>().ReverseMap();
+    }
+}
