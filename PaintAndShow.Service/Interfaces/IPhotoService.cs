31de94061cc07ab887e31f6d39871e10dcad309d
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 00:55:39 +0500

    PhotoService's interface added

---
+++ PaintAndShow.Service/Interfaces/IPhotoService.cs
@@ -0,0 +1,12 @@
+﻿using PaintAndShow.Service.DTOs.Photos;
+
+namespace PaintAndShow.Service.Interfaces;
+
+public interface IPhotoService
+{
+    Task<PhotoResultDto> AddAsync(PhotoCreationDto dto);
+    Task<PhotoResultDto> ModifyAsync(PhotoUpdateDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<PhotoResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<PhotoResultDto>> RetrieveAllAsync();
+}
