4d70c13605b95b2904dbfbdb0dd0a1ae5fb858e8
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-17 22:58:04 +0500

    Friend's entity added

---
+++ PaintAndShow.Domain/Entities/Friend.cs
@@ -0,0 +1,9 @@
+﻿using PaintAndShow.Domain.Commons;
+
+namespace PaintAndShow.Domain.Entities;
+
+public class Friend : Auditable
+{
+    public long UserId { get; set; }
+    public long FriendsId { get; set; }
+}
