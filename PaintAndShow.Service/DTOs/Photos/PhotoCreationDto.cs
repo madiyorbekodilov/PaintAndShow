300cac2069c76bc0605b0310eb381c416485f65c
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 00:00:44 +0500

    Photo's dto added

---
+++ PaintAndShow.Service/DTOs/Photos/PhotoCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PaintAndShow.Service.DTOs.Photos;
+
+public class PhotoCreationDto
+{
+    public string PhotoName { get; set; }
+    public string? Description { get; set; }
+    public string Path { get; set; }
+}
---
+++ PaintAndShow.Service/DTOs/Photos/PhotoUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PaintAndShow.Service.DTOs.Photos;
+
+public class PhotoUpdateDto
+{
+    public long Id { get; set; }
+    public string PhotoName { get; set; }
+    public string? Description { get; set; }
+    public string Path { get; set; }
+}
