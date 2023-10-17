5d1e17bbe4b3e628d2c36ab9181880be0adb83a1
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-17 23:19:39 +0500

    configuration has been corrected

--- PaintAndShow.WebApi/Program.cs
+++ PaintAndShow.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using PaintAndShow.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
