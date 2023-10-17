5de9645e6d266fb1d1b43a9f6c3266e2938f2f54
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 00:52:12 +0500

    UserService's Interface added

---
+++ PaintAndShow.Service/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using PaintAndShow.Service.DTOs.Users;
+
+namespace PaintAndShow.Service.Interfaces;
+
+public interface IUserService
+{
+    Task<UserResultDto> AddAsync(UserCreationDto dto);
+    Task<UserResultDto> ModifyAsync(UserUpdateDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<UserResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<UserResultDto>> RetrieveAllAsync();
+}
