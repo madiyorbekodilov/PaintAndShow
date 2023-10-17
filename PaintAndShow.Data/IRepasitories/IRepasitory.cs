94848cbfc21b7aad7498d04a5c6b70782ec6be05
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-17 23:29:53 +0500

    Repasitories Interface added

---
+++ PaintAndShow.Data/IRepasitories/IRepasitory.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using PaintAndShow.Domain.Commons;
+
+namespace PaintAndShow.Data.IRepasitories;
+
+public interface IRepasitory<T> where T : Auditable
+{
+    Task CreateAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<T> SelectAsync();
+    IQueryable<T> SelectAll();
+    Task SaveAsync();
+}
