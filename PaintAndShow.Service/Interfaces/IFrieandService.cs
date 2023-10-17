0d16e35c021202c1c8a215f9c542c138c23c0770
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 01:01:40 +0500

    FriendService's interface added

---
+++ PaintAndShow.Service/Interfaces/IFrieandService.cs
@@ -0,0 +1,10 @@
+﻿using PaintAndShow.Domain.Entities;
+
+namespace PaintAndShow.Service.Interfaces;
+
+public interface IFrieandService
+{
+    Task<User> AddAsync(string username);
+    Task<bool> RemoveAsync(long username);
+    Task<IEnumerable<User>> RetrieveAllAsync();
+}
