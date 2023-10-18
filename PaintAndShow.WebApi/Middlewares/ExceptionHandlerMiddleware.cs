482fd6bee7d91d83bcc739a57c191c7597a46fd2
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 10:24:15 +0500

    Middlewares added

---
+++ PaintAndShow.WebApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using PaintAndShow.Service.Exceptions;
+using PaintAndShow.WebApi.Models;
+
+namespace PaintAndShow.WebApi.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    public readonly RequestDelegate request;
+    public readonly ILogger<ExceptionHandlerMiddleware> logger;
+    public ExceptionHandlerMiddleware(RequestDelegate request, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        this.request = request;
+        this.logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await this.request(context);
+        }
+        catch (NotFoundException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (AlreadyExistException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (CustomException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            this.logger.LogError(ex.ToString());
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+    }
+}
