98750f5db60da386ae5f4fcae9c9c75f1f109e52
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-18 22:21:05 +0500

    Photos Controlles added

---
+++ PaintAndShow.WebApi/Controllers/PhotosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PaintAndShow.Service.DTOs.Photos;
+using PaintAndShow.Service.Interfaces;
+using PaintAndShow.WebApi.Models;
+
+namespace PaintAndShow.WebApi.Controllers;
+
+public class PhotosController : BaseController
+{
+    private readonly IPhotoService photoService;
+    public PhotosController(IPhotoService photoService)
+    {
+        this.photoService = photoService;
+    }
+
+    [Authorize]
+    [HttpPost("UploadPhoto")]
+    public async Task<IActionResult> UploadPhoto(PhotoCreationDto path)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = this.photoService.AddAsync(path)
+        });
+}
